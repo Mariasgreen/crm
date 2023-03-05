1bde6d8d016e849f3ce27a0af4f7ad2ca64e8337
mariiasgreen <mariiaz555@gmail.com>
2023-03-05 19:48:22 +0200

    ready

---
+++ main.js
@@ -0,0 +1,12 @@
+
+'use strict';
+
+const title = document.getElementsByClassName('modal__title');
+const form = document.getElementsByClassName('overlay__modal');
+const checkbox = document.getElementsByName('discount');
+const promocode = document.getElementsByName('discount_count');
+
+console.log(title);
+console.log(form);
+console.log(checkbox);
+console.log(promocode);
